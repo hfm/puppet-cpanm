2af11f52208f987b4f2ef0a3bffc22d8fe2e001e
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-12-14 00:01:23 +0900

    [add] install option

---
+++ lib/puppet/provider/package/cpanm.rb
@@ -0,0 +1,15 @@
+require 'puppet/provider/package'
+
+Puppet::Type.type(:package).provide(:cpanm, :parent => Puppet::Provider::Package) do
+  desc "Package provider for cpanminus"
+
+  commands :cpanm => '/usr/bin/cpanm'
+
+  def install(useversion = true)
+    command = [command(:cpanm)]
+    command << resource[:name]
+
+    output = execute(command)
+    self.fail "Could not install: #{output.chomp}" if output.include?("failed")
+  end
+end
